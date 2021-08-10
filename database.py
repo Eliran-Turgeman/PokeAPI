356b67bbd2be02f00cfc7cff79f332bb67ba2fae
Eliran <eliran9692@gmail.com>
2021-08-10 13:44:27 +0300

    update and delete functionalities added

--- database.py
+++ database.py
@@ -25,15 +25,15 @@ def load_csv_to_db():
     conn = sqlite3.connect(DB_FILENAME)
     cursor = conn.cursor()
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Pokemons (idx int, name text, type1 text,
+        CREATE TABLE IF NOT EXISTS Pokemons (name text, type1 text,
         type2 text, sum_stats int, hp int, attack int,
         special_attack int, defense int, special_defense int)
         ''')
 
     poke_data = pd.read_csv('Pokemon.csv')
-    poke_data.drop(['Speed', 'Generation', 'Legendary'], axis=1, inplace=True)
+    poke_data.drop(['#', 'Speed', 'Generation', 'Legendary'], axis=1, inplace=True)
 
-    poke_data.columns = ['idx', 'name', 'type1', 'type2', 'sum_stats',
+    poke_data.columns = ['name', 'type1', 'type2', 'sum_stats',
                          'hp', 'attack', 'special_attack', 'defense',
                          'special_defense']
 
@@ -67,4 +67,64 @@ def get_poke_by_type(type1, type2):
         SELECT * FROM Pokemons WHERE type1 = ?''', (type1,))
 
     return cursor.fetchall()
+
+
+def add_poke_to_db(name, type1, type2, sum_stats, hp, attack, special_attack,
+                   defense, special_defense):
+    conn = sqlite3.connect(DB_FILENAME)
+    cursor = conn.cursor()
+
+    if not table_exists(cursor):
+        load_csv_to_db()
+
+    cursor.execute('''
+    INSERT INTO Pokemons ('name', 'type1', 'type2', 'sum_stats',
+    'hp', 'attack', 'special_attack', 'defense', 'special_defense')
+    VALUES (?,?,?,?,?,?,?,?,?)''', (name, type1, type2, sum_stats, hp, attack,
+                                    special_attack, defense, special_defense))
+
+    conn.commit()
+
+
+def update_poke(name, type1=None, type2=None, sum_stats=None, hp=None,
+                attack=None, special_attack=None, defense=None,
+                special_defense=None):
+
+    conn = sqlite3.connect(DB_FILENAME)
+    cursor = conn.cursor()
+
+    if not table_exists(cursor):
+        load_csv_to_db()
+
+    params = [type1, type2, sum_stats, hp, attack, special_attack,
+              defense, special_defense]
+    params_names = ['type1', 'type2', 'sum_stats', 'hp', 'attack',
+                    'special_attack', 'defense', 'special_defense']
+
+    for param, param_name in zip(params, params_names):
+        if param:
+            query = '''
+            UPDATE Pokemons SET ''' + param_name + '''
+             = ? WHERE name = ?'''
+            cursor.execute(query, (param, name))
+
+    conn.commit()
+
+
+def delete_poke(name):
+    conn = sqlite3.connect(DB_FILENAME)
+    cursor = conn.cursor()
+
+    if not table_exists(cursor):
+        load_csv_to_db()
+
+    cursor.execute('''
+    DELETE FROM Pokemons WHERE name = ?''', (name,))
+
+    conn.commit()
+
+
+
+
+
 
--- pokeapi.py
+++ pokeapi.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, Path
-from pydantic import BaseModel
 from typing import Optional
-import pandas as pd
-from database import get_poke_by_name, get_poke_by_type
+
+from fastapi import FastAPI, Path, HTTPException, status
+from pydantic import BaseModel
+
+from database import get_poke_by_name, get_poke_by_type, add_poke_to_db, \
+    update_poke, delete_poke
 
 app = FastAPI()
 
@@ -21,45 +23,92 @@ class Pokemon(BaseModel):
 
 @app.get("/")
 def root():
-    return {"Message": "Nothing interesting here"}
+    raise HTTPException(status_code=status.HTTP_200_OK,
+                        detail="Welcome to PokeAPI")
 
 
 @app.get("/poke/{pokemon_name}")
 def get_pokemon_by_name(pokemon_name: str = Path(None,
-                                                 description="Name of the pokemon you'd like to retrieve")):
+                                                 description="Name of the "
+                                                             "pokemon you'd "
+                                                             "like to "
+                                                             "retrieve")):
     pokemon = get_poke_by_name(pokemon_name)
+    if not pokemon:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Pokemon not found")
 
-    return {"Pokemon": pokemon[1],
-            "Types": [pokemon[2], pokemon[3]],
-            "HP": pokemon[5],
-            "Attack": pokemon[6],
-            "Special Attack": pokemon[7],
-            "Defense": pokemon[8],
-            "Special Defense": pokemon[9],
+    return {"Pokemon": pokemon[0],
+            "Types": [pokemon[1], pokemon[2]],
+            "HP": pokemon[4],
+            "Attack": pokemon[5],
+            "Special Attack": pokemon[6],
+            "Defense": pokemon[7],
+            "Special Defense": pokemon[8],
             }
 
 
 @app.get("/poketype/{poke_type}")
 def get_pokemon_by_type(poke_type: str = Path(None,
-                                              description="Primary type of the pokemons you want to query"),
+                                              description="Primary type of "
+                                                          "the pokemons you "
+                                                          "want to query"),
                         type2: Optional[str] = None):
     pokemons = get_poke_by_type(poke_type, type2)
+    if not pokemons:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="No pokemon with this type")
     result = {}
     for idx, pokemon in enumerate(pokemons):
-        result[idx] = {"Pokemon": pokemon[1],
-            "Types": [pokemon[2], pokemon[3]],
-            "HP": pokemon[5],
-            "Attack": pokemon[6],
-            "Special Attack": pokemon[7],
-            "Defense": pokemon[8],
-            "Special Defense": pokemon[9],
-            }
+        result[idx] = {"Pokemon": pokemon[0],
+                       "Types": [pokemon[1], pokemon[2]],
+                       "HP": pokemon[4],
+                       "Attack": pokemon[5],
+                       "Special Attack": pokemon[6],
+                       "Defense": pokemon[7],
+                       "Special Defense": pokemon[8],
+                       }
 
     return result
 
 
-
 @app.post("/newPoke/{pokemon_name}")
-def create_pokemon(pokemon_name: int, pokemon: Pokemon):
-    # if pokemon_name
-    return {}
+def create_pokemon(pokemon_name: str, pokemon: Pokemon):
+    if get_poke_by_name(pokemon_name):
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                            detail="Pokemon already exists")
+    add_poke_to_db(pokemon.name, pokemon.primary_type, pokemon.secondary_type,
+                   pokemon.sum_stats, pokemon.hit_points,
+                   pokemon.attack_strength, pokemon.special_attack_strength,
+                   pokemon.defensive_strength,
+                   pokemon.special_defensive_strength)
+    raise HTTPException(status_code=status.HTTP_201_CREATED,
+                        detail="Pokemon created successfully")
+
+
+@app.put("/updatePoke/{pokemon_name}")
+def update_pokemon(pokemon_name: str, pokemon: Pokemon):
+    if not get_poke_by_name(pokemon_name):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Pokemon not found")
+
+    update_poke(pokemon.name, pokemon.primary_type, pokemon.secondary_type,
+                pokemon.sum_stats, pokemon.hit_points,
+                pokemon.attack_strength, pokemon.special_attack_strength,
+                pokemon.defensive_strength,
+                pokemon.special_defensive_strength)
+
+    raise HTTPException(status_code=status.HTTP_200_OK,
+                        detail="Pokemon details updated")
+
+
+@app.delete("/deletePoke/{pokemon_name}")
+def delete_pokemon(pokemon_name: str):
+    if not get_poke_by_name(pokemon_name):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Pokemon not found")
+
+    delete_poke(pokemon_name)
+
+    raise HTTPException(status_code=status.HTTP_200_OK,
+                        detail="Pokemon deleted successfully")
